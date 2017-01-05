69a81da5fe23da7dc7fe0c7a9f30ddf9122243a5
tsthght <781181214@qq.com>
2017-01-05 09:05:04 +0800

    修改安装脚本

--- script/dbproxy_install_local.sh
+++ script/dbproxy_install_local.sh
@@ -5,11 +5,11 @@ mkdir -p "/opt/tmp/dbproxy_log/"
 ####yum install 依赖包
 yum install -y libevent-devel lua-devel openssl-devel flex Percona-Server-devel-55.x86_64 Percona-Server-client-55.x86_64 jemalloc jemalloc-devel glib2 glib2-devel
 
-### install dbproxy
-cd $basedir/dbproxy
+### install DBProxy
+cd $basedir
 ./configure --prefix=/usr/local/mysql-proxy CFLAGS="-g -O0" && make -j 4 && make install
 
 ###mv source.cnf 
 cd $basedir
 mkdir -p /usr/local/mysql-proxy/conf
-mv ./dbproxy/script/source.cnf.samples /usr/local/mysql-proxy/conf/source.cnf
+mv ./script/source.cnf.samples /usr/local/mysql-proxy/conf/source.cnf
